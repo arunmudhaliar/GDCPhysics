d0516f786c2e06e10f603ca29e84cedecbb17684
Arun A <arun.mudhaliar@gmail.com>
2018-11-22 18:20:14 +0530

    [Ball]

---
+++ GDCUnity/Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class Ball : MonoBehaviourPunCallbacks, IPunObservable {
+    [Tooltip("The local ball instance. Use this to know if the local ball is represented in the Scene")]
+    public static GameObject localBall;
+
+    public void Awake() {
+        // #Important
+        // used in GameManager.cs: we keep track of the localPlayer instance to prevent instanciation when levels are synchronized
+        if (photonView.IsMine) {
+            localBall = gameObject;
+        }
+
+        // #Critical
+        // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void OnPhotonSerializeView( PhotonStream stream, PhotonMessageInfo info ) {
+        if (stream.IsWriting) {
+            // We own this player: send the others our data
+            //stream.SendNext(this.IsFiring);
+            //stream.SendNext(this.Health);
+        } else {
+            // Network player, receive data
+            //this.IsFiring = (bool)stream.ReceiveNext();
+            //this.Health = (float)stream.ReceiveNext();
+        }
+    }
+}
--- GDCUnity/Assets/Scripts/GameScene.cs
+++ GDCUnity/Assets/Scripts/GameScene.cs
@@ -10,11 +10,11 @@ public class GameScene : MonoBehaviour {
 
     [Tooltip("The prefab to use for representing the player")]
     [SerializeField]
-    private GameObject playerPrefab;
+    private GameObject playerPrefab = null;
 
     [Tooltip("The prefab to use for representing the ball")]
     [SerializeField]
-    private GameObject ballPrefab;
+    private GameObject ballPrefab = null;
 
     private void OnGUI()
     {
@@ -59,5 +59,25 @@ public class GameScene : MonoBehaviour {
                 clientPlayer.GetComponent<Player>().myBall = clientBall;
             }
         }
+
+
+        //if (Ball.localBall == null) {
+        //    Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
+
+        //    // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+        //    if (PhotonNetwork.IsMasterClient) {
+        //        GameObject masterPlayer = PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(-6.0f, 0f, 0f), Quaternion.identity, 0);
+        //        masterPlayer.name = "masterPlayer";
+        //        GameObject masterBall = PhotonNetwork.Instantiate(this.ballPrefab.name, new Vector3(-4.0f, 0f, 0f), Quaternion.identity, 0);
+        //        masterBall.name = "masterBall";
+        //        masterPlayer.GetComponent<Player>().myBall = masterBall;
+        //    } else {
+        //        GameObject clientPlayer = PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(6f, 0f, 0f), Quaternion.identity, 0);
+        //        clientPlayer.name = "clientPlayer";
+        //        GameObject clientBall = PhotonNetwork.Instantiate(this.ballPrefab.name, new Vector3(4.0f, 0f, 0f), Quaternion.identity, 0);
+        //        clientBall.name = "clientBall";
+        //        clientPlayer.GetComponent<Player>().myBall = clientBall;
+        //    }
+        //}
     }
 }
